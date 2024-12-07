5ac9e4a19a21ffd117364f106bd2e711e3a5cd8d
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2024-12-07 12:40:36 +0530

    Create CustomerRepository.java

---
+++ src/main/java/com/klef/jfsd/exam/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.klef.jfsd.exam.repository;
+
+import com.klef.jfsd.exam.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
